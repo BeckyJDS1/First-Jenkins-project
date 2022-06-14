79c895e386413503169fb1aa55db8d036bf6b9c7
Anastasiya Mashtakova <a.mahstakova@kpda.ru>
2022-06-14 11:13:26 +0300

    groovy

---
+++ start.groovy
@@ -0,0 +1,18 @@
+pipline {
+    agent any;
+    parameters {
+        string (name: 'STRING', defaultValue: 'Hello world', description: 'String to print')
+    }
+    stages {
+        stage('Print') {
+            step {
+                sh 'echo $STRING'
+            }
+        }
+    }
+    post {
+        always {
+            sh 'STRING="post"; echo $STRING'
+        }
+    }
+}
